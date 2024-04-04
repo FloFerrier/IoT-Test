1e2f5f234033b9e14c56517e8c174368d50a2e20
ferrierf <florian.ferrier@aol.fr>
2024-04-04 17:05:47 +0200

    Lots of stuff

--- src/database/database.py
+++ src/database/database.py
@@ -1,34 +1,38 @@
 import influxdb_client
-import random
-import time
 from influxdb_client.client.write_api import SYNCHRONOUS
 from influxdb_client.domain.write_precision import WritePrecision
 
-'''
-client = influxdb_client.InfluxDBClient(url=url, token=token, org=org)
-write_api = client.write_api(write_options=SYNCHRONOUS)
+class Database:
+    def __init__(self) -> None:
+        self.__api: dict = {
+            "url": "",
+            "token": "",
+            "org": "",
+            "bucket": ""
+        }
 
-for index in range(0, 500):
-    data = {
-        "timestamp": int(time.time()),
-        "name": "home",
-        "room": "living",
-        "type": "temperature",
-        "value": random.uniform(-25.0, 40.0),
-        "unit": "degree celsius"
-    }
-    data_formatting = "[{index}] timestamp: {timestamp} - {type}: {value} {unit}".format(index=index, timestamp=data["timestamp"], type=data["type"], value=data["value"], unit=data["unit"])
-    print(data_formatting)
+    def init(self, url: str = "", token: str = "", org: str = "", bucket: str = "") -> None:
+        self.__api["url"] = "http://{}".format(url)
+        self.__api["token"] = token
+        self.__api["org"] = org
+        self.__api["bucket"] = bucket
+        client = influxdb_client.InfluxDBClient(url=self.__api["url"], token=self.__api["token"], org=self.__api["org"])
+        self.__write_api = client.write_api(write_options=SYNCHRONOUS)
 
-    record = influxdb_client.Point.from_dict(data,
-        write_precision=WritePrecision.S,
-        record_measurement_key="name",
-        record_time_key="timestamp",
-        record_tag_keys=["room", "type", "unit"],
-        record_field_keys=["value"])
-    write_api.write(bucket=bucket, org=org, record=record)
-    time.sleep(1.0)
-'''
+    def __data_check(self, data: dict) -> bool:
+        keys = ["timestamp", "name", "location", "source", "type", "unit", "value"]
+        key_found = 0
+        for key in keys:
+            if key in data.keys():
+                key_found += 1
+        return False if key_found != len(keys) else True
+
+    def write(self, data: dict) -> bool:
+        data_is_correct: bool = self.__data_check(data)
+        if data_is_correct is True:
+            record = influxdb_client.Point.from_dict(data, write_precision=WritePrecision.S, record_measurement_key="name", record_time_key="timestamp", record_tag_keys=["location", "source", "type", "unit"], record_field_keys=["value"])
+            self.__write_api.write(bucket=self.__api["bucket"], org=self.__api["org"], record=record)
+        return data_is_correct
 
 if __name__ == "__main__":
     pass
--- src/main.py
+++ src/main.py
@@ -1,22 +1,60 @@
-from src.config.config import config_read
+from config import config_read
 from openweathermap import OpenWeatherMap
+from database import Database
 
 def main() -> None:
     config = config_read("config.yaml")
 
     weather = OpenWeatherMap()
-    response = weather.init(config["openweathermap"]["url"], config["openweathermap"]["key"], config["openweathermap"]["city"])
+    database = Database()
 
+    response = weather.init(config["openweathermap"]["url"], config["openweathermap"]["key"], config["openweathermap"]["city"])
     if response["error"]["code"] != 200:
         print("Fail to init OpenWeatherMap [{}]:\"{}\"".format(response["error"]["code"], response["error"]["message"]))
         exit(0)
 
-    data = weather.weather_get()
+    database.init(config["influxdb"]["url"], config["influxdb"]["token"], config["influxdb"]["org"], config["influxdb"]["bucket"])
+
+    response = weather.weather_get()
     if response["error"]["code"] != 200:
         print("Fail to get data OpenWeatherMap [{}]:\"{}\"".format(response["error"]["code"], response["error"]["message"]))
         exit(0)
 
-    print(response)
+    measurements = [
+        {
+            "timestamp": int(response["timestamp"]),
+            "name": "home",
+            "location": "outside",
+            "source": "openweathermap-api",
+            "type": "temperature",
+            "unit": "degree celsius",
+            "value": int(response["data"]["temperature"]),
+        },
+        {
+            "timestamp": int(response["timestamp"]),
+            "name": "home",
+            "location": "outside",
+            "source": "openweathermap-api",
+            "type": "pressure",
+            "unit": "hPa",
+            "value": int(response["data"]["pressure"]),
+        },
+        {
+            "timestamp": int(response["timestamp"]),
+            "name": "home",
+            "location": "outside",
+            "source": "openweathermap-api",
+            "type": "humidity",
+            "unit": "%",
+            "value": int(response["data"]["humidity"]),
+        }
+    ]
+
+    for data in measurements:
+        print(data)
+        response = database.write(data)
+        if response is not True:
+            print("Fail to write data")
 
 if __name__ == "__main__":
     main()
